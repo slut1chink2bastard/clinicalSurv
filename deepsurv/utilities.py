7d09ab9e999d81940a2d56b5dd3fca43362f0436
greatestslut <bestbryan13@Gmail.com>
2022-06-02 17:53:22 +0800

    add is_integer and map_breast_surg_type for transforming the column "RX Summ--Surg Prim Site (1998+)" in the breast.csv

--- deepsurv/utilities.py
+++ deepsurv/utilities.py
@@ -66,6 +66,9 @@ def filter_col_data(data, cols_array):
 def is_data_frame(data):
     return isinstance(data, pd.DataFrame)
 
+def is_integer(var):
+    return isinstance(var, int)
+
 
 def is_valid_list(array):
     return isinstance(array, list) and array
@@ -78,6 +81,44 @@ def one_hot_encode_cols(data, cols):
         raise ValueError(form_error_msg("Invalid parameter cols."))
     transformer = make_column_transformer((OneHotEncoder(), cols), remainder="passthrough")
     return transformer.fit_transform(data)
+
+def map_breast_surg_type(code):
+    '''
+    this map method is based on the Surgery_Codes_Breast_2021.pdf from https://seer.cancer.gov/archive/manuals/2021/AppendixC/Surgery_Codes_Breast_2021.pdf
+    TODO:unit test in the future
+    '''
+    if is_integer(code) is False:
+        raise ValueError(form_error_msg("Invalid parameter code."))
+    elif code == 0:
+        return "None"
+    elif code == 19:
+        return "Local tumor destruction"
+    elif code == 20 or code == 21 or code == 22 or code == 23 or code == 24:
+        return "Partial mastectomy"
+    elif code == 30:
+        return "Subcutaneous mastectomy"
+    elif code == 40 or code == 41 or code == 43 or code == 44 or code == 45 or code == 46 or code == 42 or code == 47 or code == 48 or code == 49 or code == 75:
+        return "Total (simple) mastectomy"
+    elif code == 76:
+        return "Bilateral mastectomy"
+    elif code == 50 or code == 51 or code == 53 or code == 54 or code == 55 or code == 56 or code == 52 or code == 57 or code == 58 or code == 59 or code == 63:
+        return "Modified radical mastectomy"
+    elif code == 60 or code == 61 or code == 64 or code == 65 or code == 66 or code == 67 or code == 62 or code == 68 or code == 69 or code == 73 or code == 74:
+        return "Radical mastectomy"
+    elif code == 70 or code == 71 or code == 72:
+        return "Extended radical mastectomy"
+    elif code == 80:
+        return "Mastectomy"
+    else:
+        raise ValueError(form_error_msg("Invalid parameter code."))
+
+
+
+
+
+
+
+
 
 
 
